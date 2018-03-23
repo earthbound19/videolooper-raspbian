734b5ea53a791c7157f5cfac127db408171db11b
Alex Hall <ansible1@earthbound.io>
2018-03-23 09:38:24 -0600

    dev snapshot -- doesn't work yet

--- rollYourOwn.sh
+++ rollYourOwn.sh
@@ -1,9 +1,15 @@
 # DESCRIPTION
+# WORK IN PROGRESS. Debugging on a live Pi . . .
 # Transforms a new Raspbian OS (Raspberry Pi Operating System) install into a video kiosk that auto-plays videos from a thumb drive and/or /home/pi/video. Built from commands given at: https://github.com/earthbound19/videolooper-raspbian/blob/master/README.md
 
 # USAGE
-# wget https://raw.githubusercontent.com/earthbound19/videolooper-raspbian/master/rollYourOwn.sh
+# Obtain this script via these commands:
+# wget http://s.earthbound.io/piVidSetup
+# mv piVidSetup rollYourOwn.sh
 # chmod +x ./rollYourOwn.sh
+# -- then invoke this script with:
+# ./rollYourOwn.sh
+# -- and follow the instructions in the comments at the end of the script.
 
 # CODE
 sudo apt-get update
@@ -14,9 +20,12 @@ mkdir /home/pi/video
 cd /home/pi
 wget https://raw.githubusercontent.com/earthbound19/videolooper-raspbian/master/startvideo.sh
 chmod uga+rwx startvideo.sh
-echo \"/home/pi/startvideo.sh" | tee -a /home/pi/.bashrc
-sudo raspi-config
-# Select option: 3 Boot Options
+echo \"/home/pi/startvideo.sh\" | tee -a /home/pi/.bashrc
+
+# AFTER RUNNING THIS SCRIPT, RUN:
+# sudo raspi-config
+# And select and configure thusly:
+# Boot Options -> 
 # Select option: B2 Console Autologin
-sudo raspi-config
-# Select option: 1 Expand Filesystem
+# And:
+# Expand Filesystem
